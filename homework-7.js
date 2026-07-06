ff9eed3bce3c6589ad283061fc57fb0915674d49
Ramazan Uzdenov <uzdenr7001@gmail.com>
2026-07-06 22:52:08 +0300

    Fix error

--- homework-7.js
+++ homework-7.js
@@ -1,38 +1,38 @@
 function weatherComputation (city, deg) {
   console.log(`Сейчас в ${city} температура - ${deg} градусов по Цельсию`)
-}
-weatherComputation('Ростове-на-Дону', '+27')
+};
+weatherComputation('Ростове-на-Дону', '+27');
 
-const speedLight = 299792458 
+const  SPEED_LIGHT = 299792458 ;
 
-function calculateSpeed (speed) {
-  if (speed > speedLight) {
+function compareSpeed(speed) {
+  if (speed > SPEED_LIGHT) {
     console.log('Сверхсветовая скорость')
-  }  else if (speed < speedLight) {
+  }  else if (speed < SPEED_LIGHT) {
     console.log('Субсветовая скорость')
-  } else if (speed === speedLight) {
+  } else if (speed === SPEED_LIGHT) {
     console.log('Скорость света')
   }
-}
-calculateSpeed(299792458)
+};
+compareSpeed(299792458);
 
-const product = 'lemon'
-const price = 190
+const product = 'lemon';
+const price = 190;
 
-function checkPrice (budget) {
+function buyPrice(budget) {
   if (budget > price) {
     console.log(`${product} приобретён. Спасибо за покупку!`)
   } else if (budget < price) {
     console.log(`Вам не хватает ${price - budget}$, пополните баланс`)
   }
-}
-checkPrice(100)
+};
+buyPrice(100);
 
-const name = 'Ramazan'
-const age = 23
-const student = true
+const name = 'Ramazan';
+const age = 23;
+const student = true;
 
-function checkAge () {
+function checkAge() {
   if (age > 18) {
     console.log('Совершеннолетний!')
   } else if (age < 18) {
@@ -42,5 +42,5 @@ function checkAge () {
   } else {
     console.log('Вы ввели некорекктный возраст!')
   }
-}
-checkAge()
+};
+checkAge();
