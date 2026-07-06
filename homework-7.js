c6cecc37572b148f25d2b52bec653b4ca4eaa857
Ramazan Uzdenov <uzdenr7001@gmail.com>
2026-07-06 22:30:41 +0300

    homework- 7

---
+++ homework-7.js
@@ -0,0 +1,46 @@
+function weatherComputation (city, deg) {
+  console.log(`Сейчас в ${city} температура - ${deg} градусов по Цельсию`)
+}
+weatherComputation('Ростове-на-Дону', '+27')
+
+const speedLight = 299792458 
+
+function calculateSpeed (speed) {
+  if (speed > speedLight) {
+    console.log('Сверхсветовая скорость')
+  }  else if (speed < speedLight) {
+    console.log('Субсветовая скорость')
+  } else if (speed === speedLight) {
+    console.log('Скорость света')
+  }
+}
+calculateSpeed(299792458)
+
+const product = 'lemon'
+const price = 190
+
+function checkPrice (budget) {
+  if (budget > price) {
+    console.log(`${product} приобретён. Спасибо за покупку!`)
+  } else if (budget < price) {
+    console.log(`Вам не хватает ${price - budget}$, пополните баланс`)
+  }
+}
+checkPrice(100)
+
+const name = 'Ramazan'
+const age = 23
+const student = true
+
+function checkAge () {
+  if (age > 18) {
+    console.log('Совершеннолетний!')
+  } else if (age < 18) {
+    console.log('Несовершеннолетний!')
+  } else if (age === 0) {
+    console.log('Такого возраста не существует!')
+  } else {
+    console.log('Вы ввели некорекктный возраст!')
+  }
+}
+checkAge()
